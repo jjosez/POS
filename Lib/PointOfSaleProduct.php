172a6c0ac9073a6a9393c19a21156c42a871cf89
Francesco Carmagnola <fracarma87@gmail.com>
2025-11-27 13:31:22 +0100

    Add ('P.sevende', true) condition

--- Lib/PointOfSaleProduct.php
+++ Lib/PointOfSaleProduct.php
@@ -75,7 +75,8 @@ class PointOfSaleProduct
         $where = [
             new DataBaseWhere('V.codbarras', $text, 'LIKE'),
             new DataBaseWhere('V.referencia', $text, 'LIKE', 'OR'),
-            new DataBaseWhere('P.descripcion', $text, 'XLIKE', 'OR')
+            new DataBaseWhere('P.descripcion', $text, 'XLIKE', 'OR'),
+            new DataBaseWhere('P.sevende', true)
         ];
 
         if ($company) {
